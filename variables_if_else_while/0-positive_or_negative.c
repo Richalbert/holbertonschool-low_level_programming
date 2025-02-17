e3f88797646e478e52854fab9c333b801adf76a7
Richalbert <richalbert@free.fr>
2025-02-17 16:14:22 +0100

    FIX correction pour Betty pas de spaces en debut de ligne

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,29 +16,28 @@
  */
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-	/* your code goes there */
+/* your code goes there */
 
-    if (n >= 0)
-    {
+if (n >= 0)
+{
 	if (n == 0)
-        {
-            printf("%d is zero\n", n);
-        }
-        else
-        {
-            printf("%d is positive\n", n);
-        } 
-    } 
-    else 
-    {
-        printf("%d is negative\n", n);
-    } 
-        
-        
-	return (0);
+	{
+	printf("%d is zero\n", n);
+	}
+	else
+	{
+	printf("%d is positive\n", n);
+	}
+}
+else
+{
+printf("%d is negative\n", n);
+}
+
+return (0);
 }
