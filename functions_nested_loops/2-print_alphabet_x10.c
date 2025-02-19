0e3065ad382fcdc5a1c1806d6c22273864727002
Richalbert <richalbert@free.fr>
2025-02-19 14:38:51 +0100

    ADD (LLD) affiche 10 fois l'alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,32 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_alphabet_x10 - imprime 10 fois l'alphabet
+ * print_alphabet - imprime le 26 lettres de l'alphabet en minuscule
+ *
+ * Return: rien
+ */
+void print_alphabet(void)
+{
+	char lettre;
+
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
+		_putchar(lettre);
+
+	lettre = '\n';
+	_putchar(lettre);
+}
+
+/**
+ * print_alphabet_x10 - imprime 10 lignes d'alphabet
  *
  * Return: rien
  */
 void print_alphabet_x10(void)
 {
-	int nb_fois;
+	int i;
 
-	for (nb_fois = 0; nb_fois < 10; nb_fois++)
+	for (i = 0; i < 10; i++)
 		print_alphabet();
+
 }
 
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,13 +1,8 @@
-#ifndef __0_PUTCHAR_H
-#define __0_PUTCHAR_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
-/* definition du prototype de la fonction _putchar() */
-int _putchar(char c);
-
-/* definition du prototype de la fonction print_alphabet() */
+int _putchar(char);
 void print_alphabet(void);
-
-/* definition du prototype de la fonction print_alphabet_x10() */
 void print_alphabet_x10(void);
 
 #endif
