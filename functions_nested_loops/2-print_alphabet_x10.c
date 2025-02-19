3e914982d416a876374bbc44b51ee4b0e3389622
Richalbert <richalbert@free.fr>
2025-02-19 11:46:13 +0100

    ADD (LLD) le corps de la fonction

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - imprime 10 fois l'alphabet
+ *
+ * Return: rien
+ */
+void print_alphabet_x10(void)
+{
+	int nb_fois;
+
+	for (nb_fois = 0; nb_foix < 10; nb_fois++)
+		print_alphabet();
+}
+
