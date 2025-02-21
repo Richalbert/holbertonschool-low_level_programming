ee98dcfddf348e529dcd9e4e993f370c8ab36596
Richalbert <richalbert@free.fr>
2025-02-21 15:35:53 +0100

    ADD (LLP) le decompte de jack bauer

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ *		starting from 00:00 to 23:59.
+ *
+ */
+void jack_bauer(void)
+{
+	int heure;
+	int heure2;
+	int minute;
+	int minute2;
+
+	for (heure = 0; heure <= 2; heure++)
+	{
+		for (heure2 = 0; heure2 <= 9; heure2++)
+		{
+			for (minute = 0; minute <= 5; minute++)
+			{
+				for (minute2 = 0; minute2 <= 9; minute2++)
+				{
+					_putchar(heure + '0');
+					_putchar(heure2 + '0');
+					_putchar(':');
+					_putchar(minute + '0');
+					_putchar(minute2 + '0');
+					_putchar('\n');
+				}
+			}
+		}
+
+
+	}
+}
