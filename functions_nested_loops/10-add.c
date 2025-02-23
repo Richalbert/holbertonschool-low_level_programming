552ffa893265cfc35904bc9f9e6fdfa85bbda373
Richalbert <richalbert@free.fr>
2025-02-23 14:28:58 +0100

    ADD (LLP) la somme de 2 entiers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @nombre1: le premier entier
+ * @nombre2: le deuxieme entier
+ *
+ * Return: la somme de ces 2 nombres
+ */
+int add(int nombre1, int nombre2)
+{
+	return (nombre1 + nombre2);
+}
