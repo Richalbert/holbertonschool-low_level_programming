939e81ffc34ffebeb1783e87a76aa0149c167c9a
Richalbert <richalbert@free.fr>
2025-02-23 14:58:33 +0100

    FIX (LLP) boucles croissante et decroissante

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,10 +11,24 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i < 98; i++)
+	if (n < 98)
 	{
-		printf("%d, ", i);
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("%d\n", 98);
 	}
 
-	printf("%d\n", 98);
+	if (n == 98)
+		printf("%d\n", 98);
+
+	if (n > 98)
+	{
+		for (i = n; i > 97; i--)
+		{
+			printf("%d, ", i);
+		}
+		printf("%d\n", 98);
+	}
 }
