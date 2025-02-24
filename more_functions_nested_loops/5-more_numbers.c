3f8751c7c474d85e41bbe7e5e7dc029e6ed1e1ed
Richalbert <richalbert@free.fr>
2025-02-24 14:29:12 +0100

    ADD affiche dix lignes

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void)
+{
+	int i;
+	int unite, dizaine;
+	int ligne;
+
+	for (ligne = 0; ligne < 10; ligne++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				unite = i % 10;
+				dizaine = (i -  unite) / 10;
+				_putchar('0' + dizaine);
+				_putchar('0' + unite);
+			}
+			else
+			{
+				_putchar('0' + i);
+			}
+		}
+		_putchar('\n');
+	}
+}
