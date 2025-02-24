a30cf79e266d02076d212a1f172e199b3fbbdd4c
Richalbert <richalbert@free.fr>
2025-02-24 10:54:22 +0100

    ADD multiplie 2 nombres

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul -	multiplies two integers
+ *
+ * @a:		le 1er nombre a multiplier
+ * @b:		le 2nd nombre a multiplier
+ *
+ * Return:	le resultat de la multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
