d50eb860e8fa01db439619f09a56be1d2aa18b41
Richalbert <richalbert@free.fr>
2025-02-26 11:14:02 +0100

    FIX test7 limitation des chaines a 509 -1

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -69,7 +69,7 @@ void puts2(char *str)
 	len = _strlen(ptr);
 
 	if (len > LIMITATION_C89_LONGUEUR_STRING)
-		len = LIMITATION_C89_LONGUEUR_STRING;
+		len = LIMITATION_C89_LONGUEUR_STRING - 1;
 
 	for (i = 0; i < len; i++)
 	{
